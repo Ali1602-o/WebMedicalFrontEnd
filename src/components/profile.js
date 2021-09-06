e480b4a0d28f74939e7c8171a97e439028ca61f7
Ali1602 <ali.oumlal.16@gmail.com>
2021-09-06 15:16:59 +0000

    profile page modifications

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,14 +1,90 @@
 import React, { Component } from "react";
 import AuthService from "../services/auth-service";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+
+
+const required = value => {
+  if (!value) {
+    return (
+      <div class="input-error-message">
+        * Ce champ est obligatoire !
+      </div>
+    );
+  }
+};
+
+const vtelephone = value => {
+  const teleRegx = /^0(5|6|7)[0-9]{8}$/ 
+  if (!value.match(teleRegx)) {
+    return (
+      <div class="input-error-message">
+        * invalide (10 nombres, ex : 0612325689)
+      </div>
+    );
+  }
+};
+
+const vname = value => {
+  var unameRegex = /^[A-Za-z ]+$/;
+  if (!value.match(unameRegex) || value.length < 3) {
+    return (
+      <div class="input-error-message">
+        * format invalide 
+      </div>
+    );
+  }
+};
 
 export default class Profile extends Component {
   constructor(props) {
     super(props);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.onChangeNom = this.onChangeNom.bind(this);
+    this.onChangePrenom = this.onChangePrenom.bind(this);
+    this.onChangeDtNaissance = this.onChangeDtNaissance.bind(this);
+    this.onChangeTelephone = this.onChangeTelephone.bind(this);
 
     this.state = {
-      currentUser: AuthService.getCurrentUser()
+      currentUser: AuthService.getCurrentUser(),
+      prenom : "",
+      nom : "",
+      telephone : "",
+      dtNaissance : null,
+      successful: false,
+      message: ""
     };
   }
+  
+  onChangeNom(e) {
+    this.setState({
+      nom: e.target.value
+    });
+  }
+
+  onChangePrenom(e) {
+    this.setState({
+      prenom: e.target.value
+    });
+  }
+
+  onChangeDtNaissance(e) {
+    this.setState({
+      dtNaissace: e.target.value
+    });
+  }
+
+  onChangeTelephone(e){
+    this.setState({
+      telephone: e.target.value
+    })
+  }
+
+  handleEdit(e){
+    e.preventDefault();
+    this.form.validateAll();
+  }
+
 
   render() {
     const { currentUser } = this.state;
@@ -38,22 +114,69 @@ export default class Profile extends Component {
                                                   <p class="m-b-10 f-w-600">Email</p>
                                                   <h6 class="text-muted f-w-400">{currentUser.email}</h6>
                                               </div>
-                                              <div class="col-sm-6">
-                                                  <p class="m-b-10 f-w-600">Telephone</p>
-                                                  <h6 class="text-muted f-w-400">98979989898</h6>
-                                              </div>
                                           </div>
-                                          <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
-                                          <div class="row">
-                                              <div class="col-sm-6">
-                                                  <p class="m-b-10 f-w-600">Recent</p>
-                                                  <h6 class="text-muted f-w-400">Sam Disuja</h6>
-                                              </div>
-                                              <div class="col-sm-6">
-                                                  <p class="m-b-10 f-w-600">Most Viewed</p>
-                                                  <h6 class="text-muted f-w-400">Dinoter husainm</h6>
-                                              </div>
-                                          </div>
+                                          <Form 
+                                            onSubmit={this.handleEdit} 
+                                            ref={c => {
+                                              this.form = c;
+                                            }}
+                                          >
+                                            <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Completer votre profil</h6>
+                                            <div class="row">
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Prénom</p>
+                                                    <Input
+                                                      type="text"
+                                                      className="form-control"
+                                                      name="prenom"
+                                                      placeholder="Entrez votre prénom"
+                                                      value={this.state.prenom}
+                                                      onChange={this.onChangePrenom}
+                                                      validations={[required,vname]}
+                                                    />
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Nom</p>
+                                                    <Input
+                                                      type="text"
+                                                      className="form-control"
+                                                      name="nom"
+                                                      placeholder="Entrez votre nom"
+                                                      value={this.state.nom}
+                                                      onChange={this.onChangeNom}
+                                                      validations={[required,vname]}
+                                                    />
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Date de naissance</p>
+                                                    <Input 
+                                                      type="date"
+                                                      className="form-control"
+                                                      name="dtNaissance"
+                                                      value={this.state.dtNaissance}
+                                                      onChange={this.onChangeDtNaissance}
+                                                      validations={[required]}
+                                                    />
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Telephone</p>
+                                                    <Input
+                                                      type="text"
+                                                      className="form-control"
+                                                      name="telephone"
+                                                      placeholder="Entrez votre numero telephone"
+                                                      value={this.state.telephone}
+                                                      onChange={this.onChangeTelephone}
+                                                      validations={[required,vtelephone]}
+                                                    />
+                                                </div>
+                                            </div><br/>
+                                            <button
+                                              className="btn btn-primary btn-block login-button"
+                                            >
+                                              <span>Add</span>
+                                            </button>
+                                          </Form>
                                       </div>
                                   </div>
                               </div>
