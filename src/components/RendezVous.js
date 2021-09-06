2382ec2c65be0bc28f9954fc4b9a27d04adece75
soukainafathii <soukainafathi2525@gmail.com>
2021-09-06 17:07:31 +0200

    blank rendezvous page

---
+++ src/components/RendezVous.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import Form from "react-validation/build/form";
+
+
+class RendezVous extends Component {
+
+  render() {
+    return (
+      <h1>hello world</h1>
+
+
+    );
+  }
+} export default RendezVous;
